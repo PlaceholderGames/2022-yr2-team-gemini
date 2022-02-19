4d0130b63a5431a3d7eca06d7446dd484fe25fcb
SeanLilley1 <30002253@students.southwales.ac.uk>
2022-02-19 14:29:19 +0000

    First Puzzle with collider issue

--- Assets/Scripts/switchCameras.cs
+++ Assets/Scripts/switchCameras.cs
@@ -9,15 +9,34 @@ public class switchCameras : MonoBehaviour
 
     public GameObject camOne;
     public GameObject camTwo;
+<<<<<<< Updated upstream
     private GameObject[] currentCam;
     public int iterator;
+=======
+    public GameObject camThree;
+    public GameObject camFour;
+    public List<GameObject> cameras;
+    public int i;
+    public int sizeCheck;
+    private int maxCameras;
+>>>>>>> Stashed changes
 
     private void Start()
     {
         camOne.SetActive(true);
+<<<<<<< Updated upstream
         currentCam[0] = camOne;
         currentCam[1] = camTwo;
         iterator = 0;
+=======
+        cameras[0] = camOne;
+        cameras[1] = camTwo;
+        cameras[2] = camThree;
+        cameras[3] = camFour;
+        i = 0;
+        maxCameras = cameras.Count;
+        sizeCheck = i + 1;
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
